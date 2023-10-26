dbd6f2ececeed37f35862a101ad8716edc858288
Riyazo4 <mohammedriyazriyaz04@gmail.com>
2023-10-27 02:47:12 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-[100vh] flex flex-col justify-center items-center">
+      <div className="w-[350px]">
+        <form>
+          <input type="text" />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
